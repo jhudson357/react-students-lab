aa4a3e8901567529097015e9311e878a9d23caca
James Hudson <jmh357@comcast.net>
2022-10-11 16:08:36 -0500

    add score component

---
+++ src/components/Score/Score.jsx
@@ -0,0 +1,10 @@
+const Score = (props) => {
+  return (  
+    <div>
+      <h3>Date: {props.score.date}</h3>
+      <h3>Score: {props.score.score}</h3>
+    </div>
+  );
+}
+
+export default Score;
--- src/components/Student/Student.jsx
+++ src/components/Student/Student.jsx
@@ -1,7 +1,13 @@
+import Score from "../Score/Score";
+
 const Student = (props) => {
   return (  
     <div>
       <h1>{props.student.name}</h1>
+      <h2>{props.student.bio}</h2>
+      {props.student.scores.map(score => 
+        <Score key={score.date} score={score} />  
+      )}
     </div>
   );
 }
